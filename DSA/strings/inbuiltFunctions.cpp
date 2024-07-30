1dee8bee9ff70cea39f1681eade93b4e2ab501c4
Vansh <123320455+vansh-08@users.noreply.github.com>
2024-07-30 21:02:42 +0530

    Add files via upload

---
+++ DSA/strings/inbuiltFunctions.cpp
@@ -0,0 +1,11 @@
+/*
+    Commonly used inbuilt funcitons ->
+ * reverse()
+ * substr()
+ * "+" operator
+ * strcat()
+ * push_back()
+ * size()
+ * to_string()
+
+*/
---
+++ DSA/strings/stringBasic.cpp
@@ -0,0 +1,68 @@
+/**
+ * strings are objects of a class std: string  in c++ (char ch = 'a';)
+ * used to represent sequence of characters.
+ * #include<string> (header for string)...
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+// int main(){
+    
+//     string str = "Vansh";
+
+//     string str1("Sharma");
+
+//     cout<<str<<" "<<str1<<endl;
+//     cout<<"HELLO"<<endl;
+
+//     string input;
+    // cin>>input;   // cin>> (only takes input until it gets space or tab or newline) and terminate input after getting any of given element i.e. we cannot give input with a space or tab or newline using cin>> method... 
+    // // space, tab, newline works as terminating character in cin>> input method...
+
+//     getline(cin, input);
+//     cout<<input<<endl;
+
+//     // // Indexing of characters in a string(similar to arrays)
+
+//     string name = "VANSH SHARMA";
+//     cout<<name[3];
+
+//     return 0;
+// }
+
+
+
+/**
+ * ASCII values
+ * characters has a numerical value
+*/
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch = 'a';
+//     cout<<int(ch)<<endl;
+
+//     return 0;
+// }
+
+
+
+
+/**
+ * String vs Character array...
+
+ *          String ->
+ * string is a class and string variables(str1, str2) are objects of this class
+ * dynamic memory allocation
+ * no pre-allocated memory
+ * have inbuilt functions
+
+ *     Character Array ->
+ * array of char data type
+ * char array_name[size];
+ * static memory allocation
+ * unused allocated memory is wasted
+ * faster
+
+ */
